e56dbbf96be426fd859bb7ac8f7ba1e62dca3625
FG Ribreau <github@fgribreau.com>
2015-05-15 17:24:57 +0200

    fix(strategy): handle timestamp as string

--- test/CleverCloud.verify.test.js
+++ test/CleverCloud.verify.test.js
@@ -1,63 +1,87 @@
-var crypto              = require('crypto')
-  , CleverCloudStrategy = require('../lib/strategy')
+'use strict';
 
-describe('CleverCloudStrategy', function() {
-  var USER_EMAIL     = 'test@test.com';
-  var USER_ID        = 10;
-  var USERS          = {};
-  var SSO_SALT       = 'secret';
+var crypto = require('crypto');
+var CleverCloudStrategy = require('../lib/strategy');
 
-  USERS[USER_EMAIL]  = {id:USER_ID, plop:true};
+describe('CleverCloudStrategy', function () {
+  var USER_EMAIL = 'test@test.com';
+  var USER_ID = 10;
+  var USERS = {};
+  var SSO_SALT = 'secret';
+  var TOKEN = '5557cab6315fce75f5dab448511219579096dea2';
 
-  describe('with verify callback that accepts params', function() {
+  USERS[USER_EMAIL]  = {
+    id: USER_ID,
+    plop: true
+  };
+
+  describe('with verify callback that accepts params', function () {
 
     function verify(id, email, done) {
       if (USERS.hasOwnProperty(email)) {
-        return done(null, USERS[email], { message: 'Hello' });
+        return done(null, USERS[email], {
+          message: 'Hello'
+        });
       }
 
       return done(null, false);
     }
 
-    var strategy = new CleverCloudStrategy({sso_salt: SSO_SALT}, verify);
+    var strategy = new CleverCloudStrategy({
+      sso_salt: SSO_SALT
+    }, verify);
 
-    describe('handling an authorized callback request', function() {
+    describe('handling an authorized callback request', function () {
       var request, user, info;
 
-      before(function(done) {
+      before(function (done) {
         chai.passport.use(strategy)
-          .success(function(u, i) {
+          .success(function (u, i) {
             user = u;
             info = i;
             done();
           })
-          .req(function(req) {
-            request                = req;
-            req.body              = {};
+          .req(function (req) {
+            request = req;
+            req.body = {};
 
-            var shasum             = crypto.createHash('sha1');
-            var ID                 = 'abcdefghijklmnop';
-            var reqTime            = Date.now()-100;
-            shasum.update(ID + ':' + SSO_SALT + ':' + reqTime);
-
-            req.body['id']        = ID;
-            req.body['timestamp'] = reqTime;
-            req.body['token']     = shasum.digest('hex');
-            req.body['email']     = USER_EMAIL;
+            var ID = 'abcdefghijklmnop';
+            req.body['id'] = ID;
+            req.body['timestamp'] = 1431703067707;
+            req.body['token'] = TOKEN;
+            req.body['email'] = USER_EMAIL;
           })
           .authenticate();
       });
 
-      it('should supply user', function() {
+      it('should supply user', function () {
         expect(user).to.be.an.object;
         expect(user.id).to.equal(USER_ID);
       });
 
-      it('should supply info', function() {
+      it('should supply info', function () {
         expect(info).to.be.an.object;
         expect(info.message).to.equal('Hello');
       });
+
+      it('should handle timestamp as a string', function (done) {
+        chai.passport.use(strategy)
+          .success(function (u, i) {
+            done();
+          })
+          .req(function (req) {
+            request = req;
+            req.body = {};
+            var ID = 'abcdefghijklmnop';
+            req.body['id'] = ID;
+            req.body['timestamp'] = '1431703067707';
+            req.body['token'] = TOKEN;
+            req.body['email'] = USER_EMAIL;
+          })
+          .authenticate();
+      });
     });
+
   });
 
 });
